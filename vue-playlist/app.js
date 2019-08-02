ba81eeb9c3bc6b9764dd356e8686adc8677e985c
zhangliyun <1819269990@qq.com>
2019-08-02 14:49:14 +0800

    数据和方法

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -1,6 +1,13 @@
 new Vue({
     el: '#vue-app',//需要获取的元素，一定是html中的根元素容器
     data: {//用于数据的存储
-        name: 'starbroken'
+        name: 'starbroken',
+        job:"web开发"
+    },
+    methods:{
+    	greet:function (time) {
+    		// body...
+    		return "Good "+time+" "+this.name+"!";
+    	}
     }
 });
