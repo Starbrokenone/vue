3bef0ab4d056357e08105ff3e3bc47429bfc1e41
zhangliyun <1819269990@qq.com>
2019-08-02 15:03:44 +0800

    属性绑定

--- vue-playlist/app.js
+++ vue-playlist/app.js
@@ -2,7 +2,9 @@ new Vue({
     el: '#vue-app',//需要获取的元素，一定是html中的根元素容器
     data: {//用于数据的存储
         name: 'starbroken',
-        job:"web开发"
+        job:"web开发",
+        website:"https://starbrokenone.github.io/",
+        websiteTag:'<a href="https://starbrokenone.github.io/">blog</a>'
     },
     methods:{
     	greet:function (time) {
